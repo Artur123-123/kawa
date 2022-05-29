4e49af72d0b741b11bc0aa450cae98cd1e1c7f5e
Arthur <71123876+Artur123-123@users.noreply.github.com>
2022-05-29 17:06:56 +0200

    Add files via upload

---
+++ kawa/main.js
@@ -0,0 +1,38 @@
+function kawa(){
+    let numer = document.getElementById('numer').value
+    let waga = document.getElementById('waga').value
+    let wynik = document.getElementById('wynik')
+    let koszt = 0
+
+    if(numer > 3 || numer < 1){
+        koszt = 0
+        wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+    }
+        else {
+            if(numer = 1) {
+            koszt = 5 * waga
+            wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+            }
+            if(numer = 2) {
+            koszt = 7 * waga
+            wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+            }
+            if(numer = 3) {
+            koszt = 6 * waga
+            wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+            }
+        }
+        
+    // if(numer = 1){
+    //     koszt = 5 * waga 
+    //     wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+    // }
+    // if(numer = 2){
+    //     koszt = 7 * waga
+    //     wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+    // }
+    // if(numer = 3){
+    //     koszt = 6 * waga
+    //     wynik.innerHTML='Koszt zamówienia wynosi '+koszt+' zł'
+    // }
+}
